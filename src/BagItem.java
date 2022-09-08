bfd380f35e4370b3ec751b4d464641c551b2ad2a
TheWinterDolphin <andrew.sorak@gmail.com>
2022-09-08 08:41:54 -0500

    BagItem Class, and Computer Team Constructor and Add Bag Method

---
+++ src/BagItem.java
@@ -0,0 +1,9 @@
+public class BagItem {
+    Item itemType;
+    int numOfItems;
+
+    public BagItem(Item itemType, int numOfItems) {
+        this.itemType = itemType;
+        this.numOfItems = numOfItems;
+    }
+}
--- src/ComputerTeam.java
+++ src/ComputerTeam.java
@@ -1,2 +1,21 @@
 public class ComputerTeam {
+    public Pokemon[] pokemons;
+    public BagItem[] bag;
+
+    ComputerTeam(Pokemon[] pokemons, BagItem[] bag) {
+        this.pokemons = pokemons;
+        this.bag = bag;
+    }
+
+    public void addBagItem(Item item, int numOfItems) {
+        BagItem[] newBag = new BagItem[bag.length+1];
+        for (int i=0; i<bag.length; i++) {
+            newBag[i] = bag[i];
+        }
+        newBag[bag.length] = new BagItem(item, numOfItems);
+
+        this.bag = newBag;
+    }
+
+
 }

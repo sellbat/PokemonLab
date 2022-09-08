d9a4d3ec0fbf87996608e93ebd3e682de42b9756
Sellbat <wbatsell05@gmail.com>
2022-09-08 09:19:54 -0500

    Attack Class

--- src/Attack.java
+++ src/Attack.java
@@ -1,2 +1,14 @@
 public class Attack {
+
+    public int damage;
+
+    public String name;
+
+    public Effect effect;
+
+    public Attack(int damage, String name, Effect effect){
+        this.damage = damage;
+        this.name = name;
+        this.effect = effect;
+    }
 }

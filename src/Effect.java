96a6e79de10939c6f917bfc6402b1db112b9b6a1
Sellbat <wbatsell05@gmail.com>
2022-09-08 09:05:16 -0500

    Item and effect class

--- src/Effect.java
+++ src/Effect.java
@@ -1,2 +1,6 @@
 public class Effect {
+    public String name;
+    public int roundsLeft;
+    public int damage;
+    public boolean incapacitate;
 }
--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,19 @@
 public class Item {
+    public int levelIncrease;
+    public int healthIncrease;
+    public boolean revive;
+    public boolean maxRevive;
+    public boolean pokeBall;
+    public String name;
+    public Item(int levelIncrease, int healthIncrease, boolean revive, boolean maxRevive, boolean pokeBall, String name){
+        this.levelIncrease = levelIncrease;
+        this.healthIncrease = healthIncrease;
+        this.revive = revive;
+        this.maxRevive = maxRevive;
+        this.pokeBall = pokeBall;
+        this.name = name;
+    }
+
+
+
 }

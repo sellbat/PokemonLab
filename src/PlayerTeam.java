8054e559ba52e996854562e10e7881927606f823
Sellbat <wbatsell05@gmail.com>
2022-09-08 08:42:16 -0500

    Update playerTeam

--- src/PlayerTeam.java
+++ src/PlayerTeam.java
@@ -1,2 +1,32 @@
 public class PlayerTeam {
+    public Pokemon[] pokemons;
+    public BagItem[] bag;
+
+    public void addBagItem(int amount, Item item){
+        BagItem[] newBag = new BagItem[bag.length + 1];
+        for(int i=0; i< newBag.length ; i++){
+            newBag[i] = bag[i];
+        }
+        newBag[newBag.length] = new BagItem(item, amount);
+        this.bag = newBag;
+    }
+    public Pokemon[] getPokemons() {
+        return pokemons;
+    }
+
+    public void setPokemons(Pokemon[] pokemons) {
+        this.pokemons = pokemons;
+    }
+
+    public BagItem[] getBag() {
+        return bag;
+    }
+
+    public void setBag(BagItem[] bag) {
+        this.bag = bag;
+    }
+
+
+
+
 }

854baddd0dda4b690965ec11821656cbd182c937
TheWinterDolphin <andrew.sorak@gmail.com>
2022-09-09 14:21:38 -0500

    Added Effects

--- src/Effect.java
+++ src/Effect.java
@@ -1,8 +1,51 @@
 public class Effect {
-    public String name;
-    public int roundsLeft;
-    public int damage;
-    public boolean incapacitate;
+    public enum Effects {Paralyzed, Poisoned, Burned, Frozen, Flinch};
 
-    Effect(name, roundsLeft, damage, boolean incapacitate)
+    public Effects name;
+    public int roundsLeft;
+    public double scalingDamage;
+    public int damage;
+    public double incapacitateChance;
+
+    Effect(Effects effect, int roundsLeft) {
+        this.name = effect;
+        this.roundsLeft = roundsLeft;
+
+        switch(effect) {
+            case Paralyzed:
+                this.scalingDamage = 0.0;
+                this.damage = 0;
+                this.incapacitateChance = 0.25;
+                break;
+
+            case Poisoned:
+                this.scalingDamage = 0.0625;
+                this.damage = 0;
+                this.incapacitateChance = 0.0;
+                break;
+
+            case Burned:
+                this.scalingDamage = 0.125;
+                this.damage = 0;
+                this.incapacitateChance = 0.0;
+                break;
+
+            case Frozen:
+                this.scalingDamage = 0.0;
+                this.damage = 0;
+                this.incapacitateChance = 1.0;
+                break;
+
+            case Flinch:
+                this.scalingDamage = 0.0;
+                this.damage = 0;
+                this.incapacitateChance = 1.0;
+                break;
+
+            default:
+                this.scalingDamage = 0.0;
+                this.damage = 0;
+                this.incapacitateChance = 0.0;
+        }
+    }
 }
--- src/Nature.java
+++ src/Nature.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class Nature {

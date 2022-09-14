2bbad5db34dfcbc6e7e062e09a26a8d4864b9cb6
Sellbat <wbatsell05@gmail.com>
2022-09-14 15:18:32 -0500

    computerTeam switch

--- src/Game.java
+++ src/Game.java
@@ -202,6 +202,7 @@ public class Game {
     public void switchPokemon(boolean isCompTeam, Pokemon choice) {
         if (isCompTeam) {
             compPokemon = choice;
+            System.out.println("Computer team has switched pokemon to "+ choice.getNickName());
         }
         else {
             playerPokemon = choice;
